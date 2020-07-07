a2704ba7b5bf83011627610e8f0175ac584fc3b2
Daniel Bylinka <daniel.bylinka@gmail.com>
2020-07-08 00:47:36 +0200

    Check that dwmblocks pid is correct before signaling

--- dwm.c
+++ dwm.c
@@ -1333,13 +1333,21 @@ getatomprop(Client *c, Atom prop)
 int
 getdwmblockspid()
 {
-	char buf[16];
-	FILE *fp = popen("pidof -s dwmblocks", "r");
+	char buf[19];
+	FILE *fp;
+	if (dwmblockspid) {
+		snprintf(buf, sizeof(buf), "/proc/%u/comm", dwmblockspid);
+		if ((fp = fopen(buf, "r"))) {
+			fgets(buf, sizeof(buf), fp);
+			fclose(fp);
+			if (!strncmp(buf, "dwmblocks", 9))
+				return dwmblockspid;
+		}
+	}
+	fp = popen("pidof -s dwmblocks", "r");
 	fgets(buf, sizeof(buf), fp);
-	pid_t pid = strtoul(buf, NULL, 10);
 	pclose(fp);
-	dwmblockspid = pid;
-	return pid != 0 ? 0 : -1;
+	return strtoul(buf, NULL, 10);
 }
 
 int
@@ -2285,16 +2293,10 @@ sigdwmblocks(const Arg *arg)
 {
 	union sigval sv;
 	sv.sival_int = (dwmblockssig << 8) | arg->i;
-	if (!dwmblockspid)
-		if (getdwmblockspid() == -1)
-			return;
+	if (!(dwmblockspid = getdwmblockspid()))
+		return;
 
-	if (sigqueue(dwmblockspid, SIGUSR1, sv) == -1) {
-		if (errno == ESRCH) {
-			if (!getdwmblockspid())
-				sigqueue(dwmblockspid, SIGUSR1, sv);
-		}
-	}
+	sigqueue(dwmblockspid, SIGUSR1, sv);
 }
 
 void
